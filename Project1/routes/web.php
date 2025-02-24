b55abf19b386f24317f6498b046094522f8567eb
prince <princekumar7320918928@gmail.com>
2025-02-24 10:53:36 +0530

    first commit

---
+++ Project1/routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/test',function (){
+    return "Hello";
+});
+Route::get('/test/{userName}',function ($userName){
+    return "Hello, $userName";
+});
+
+Route::get('sum/{a}/{b}',function($a,$b){
+   return "sum of $a and $b is ".$a+$b; 
+});
+
+Route::get('lpuaddmission/{regno?}',function($regno=null){
+    if($regno==null){
+        return "Error: You are not registered";
+    }
+    return "Hello Your regNo: ".$regno; 
+});
+
+//pass the data using associative array
+Route::get('test/{$name}',function($name){
+    return view('test',['name'=>$name]);
+});
+
+
+Route::get('test1/{name}/{marks}',function($name,$marks){
+    return view('test',['name'=>$name,'marks'=>$marks]);
+});
+
+//pass the data using with method
+Route::get('test2/{name}/{marks}',function($name,$marks){
+    return view('test')->with('name',$name)->with('marks',$marks);
+});
+
+Route::get('test3/{name}/{marks?}',function($name,$marks="Not evaluated yet"){
+    return view('test')->with('name',$name)->with('marks',$marks);
+});
+
+//pass the data using compact method
+Route::get('test4/{name}/{marks}',function($name,$marks){
+    return view('test', compact('name', 'marks'));
+});
---
+++ Project2_controllers/app/Http/Controllers/myController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class myController extends Controller{
+    public function display(){
+        $name = "Abhishek";
+        $marks = 28;
+        return "Hello $name Your marks are $marks";
+    }
+}
